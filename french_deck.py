0dc998cada9d1cdad62e7442c3d799a538c0de27
vsrikarunyan <vsrikarunyan@gmail.com>
2024-11-26 21:09:07 +0530

    Started with basics of OOPS in Python

---
+++ french_deck.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+Card = namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+Card.suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(Card.suit_values) + Card.suit_values[card.suit]
+Card.overall_rank = spades_high
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pysnippets!")
+
+
+if __name__ == "__main__":
+    main()
